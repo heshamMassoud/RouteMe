837857239608f095769374652406c1112f9e1a5c
Hesham Massoud <hesham.massoud@tum.de>
2016-05-21 11:47:20 +0200

    Refactor loginViewController and fix bugs and warnings

--- RouteMe/LoginViewController.swift
+++ RouteMe/LoginViewController.swift
@@ -11,60 +11,107 @@ import Alamofire
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
     @IBAction func loginAction(sender: AnyObject) {
-        var username = self.usernameField.text
-        var password = self.passwordField.text
-        
-        // Validate the text fields
-        if username?.characters.count < 5 {
-            UIAlertView(title: "Invalid", message: "Email must be greater than 5 characters", delegate: self, cancelButtonTitle: "OK").show()
-            
-        } else if password?.characters.count < 1 {
-            UIAlertView(title: "Invalid", message: "Password must be greater than 1 character", delegate: self, cancelButtonTitle: "OK").show()
-            
-        } else {
-            usernameField.resignFirstResponder()
-            passwordField.resignFirstResponder()
-            // Run a spinner to show a task in progress
-            var spinner: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
-            spinner.startAnimating()
-            
-            
-            
-            let parameters = ["email": username!, "password": password!]
-            
-            Alamofire.request(.POST, "http://routeme-api.us-east-1.elasticbeanstalk.com/api/users/login", parameters: parameters, encoding:.JSON).responseJSON
-                { response in switch response.result {
-                case .Success(let JSON):
-                    print("Success with JSON: \(JSON)")
-                    spinner.stopAnimating()
-                    let statusCode = (response.response?.statusCode)!
-                    let response = JSON as! NSDictionary
-                    if (statusCode == 302) {
-                        let response = JSON as! NSDictionary
-                        let loggedInUsername = response["username"] as! String
-                        self.loginUser(loggedInUsername)
-                        
-                    } else {
-                        let errorMessage = response["message"] as! String
-                        let errorField = response["field"] as! String
-                        UIAlertView(title: errorField, message: errorMessage, delegate: self, cancelButtonTitle: "OK").show()
-                    }
-                case .Failure(let error):
-                    print("Request failed with error: \(error)")
-                    }
-            }
+        let email = self.emailField.text
+        let password = self.passwordField.text
+        let isFormValid = validateLoginForm(email!, password: password!)
+        if isFormValid {
+            authenticateUser(email!, password: password!)
         }
     }
     
+    func authenticateUser(email: String, password: String) {
+        let spinnerFrame: UIView = startASpinner()
+        let parameters = ["email": email, "password": password]
+        Alamofire.request(
+            .POST,
+            "http://routeme-api.us-east-1.elasticbeanstalk.com/api/users/login",
+            parameters: parameters,
+            encoding:.JSON)
+            .responseJSON
+            {
+                response in
+                self.stopSpinner(spinnerFrame)
+                switch response.result {
+                    case .Success(let JSON):
+                        let HTTP_STATUS_CODE_FOUND: Int = 302
+                        let statusCode = (response.response?.statusCode)!
+                        let responseJSON = JSON as! NSDictionary
+                        if (statusCode == HTTP_STATUS_CODE_FOUND) {
+                            let loggedInUsername = responseJSON["username"] as! String
+                            self.loginUser(loggedInUsername)
+                        } else {
+                            self.alertAuthenticationError(responseJSON)
+                    }
+                    case .Failure(let error):
+                        self.alert("Fatal Error", message: "Request failed with error: \(error)", buttonText: "OK")
+                }
+        }
     }
     
+    func startASpinner() -> UIView {
+        var messageFrame = UIView()
+        var activityIndicator = UIActivityIndicatorView()
+        messageFrame = UIView(frame: CGRect(x: view.frame.midX - 25, y: view.frame.midY - 25 , width: 50, height: 50))
+        messageFrame.layer.cornerRadius = 15
+        messageFrame.backgroundColor = UIColor(white: 0, alpha: 0.7)
+        activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.White)
+        activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        activityIndicator.startAnimating()
+        messageFrame.addSubview(activityIndicator)
+        view.addSubview(messageFrame)
+        return messageFrame
+    }
+    
+    func stopSpinner(spinnerFrame: UIView) {
+        spinnerFrame.removeFromSuperview()
+    }
+    
+    func validateLoginForm(email: String, password: String) -> Bool {
+        let isValidEmail = validateEmailField(email)
+        let isValidPassword = validatePasswordField(password)
+        if !isValidEmail {
+            alert("E-mail", message: "Please enter a valid e-mail address.", buttonText: "OK")
+            return false
+        } else if !isValidPassword {
+            alert("Password", message: "Password must be greater than 1 character", buttonText: "OK")
+            return false
+        }
+        return true
+    }
+    
+    func validateEmailField(email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailEvaluator = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailEvaluator.evaluateWithObject(email)
+    }
+    
+    func validatePasswordField(password: String) -> Bool {
+        return password.characters.count > 1
+    }
+
+    func alert(title: String, message: String, buttonText: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert);
+        let alertButton = UIAlertAction(title: buttonText, style: UIAlertActionStyle.Default, handler: {(actionTarget: UIAlertAction) in })
+        alert.addAction(alertButton)
+        showViewController(alert, sender: self);
+    }
+    
+
     func loginUser(username: String) {
         self.rememberUser(username)
         self.redirectToMainView()
+    }
+    
+    func rememberUser(username: String) {
+        let hasLoginKey = NSUserDefaults.standardUserDefaults().boolForKey("isLoggedIn")
+        if hasLoginKey == false {
+            NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isLoggedIn")
+            NSUserDefaults.standardUserDefaults().setValue(username, forKey: "username")
+        }
     }
     
     func redirectToMainView() {
@@ -74,12 +121,12 @@ class LoginViewController: UIViewController {
         })
     }
     
-    func rememberUser(username: String) {
-        let hasLoginKey = NSUserDefaults.standardUserDefaults().boolForKey("isLoggedIn")
-        if hasLoginKey == false {
-            NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isLoggedIn")
-            NSUserDefaults.standardUserDefaults().setValue(username, forKey: "username")
-        }
+    func alertAuthenticationError(responseJSON: NSDictionary) {
+        let AUTHENTICATION_FIELD_ERROR_INDEX = 0
+        let AUTHENTICATION_FIELD_ERROR_ENTRY = responseJSON["fieldErrors"]![AUTHENTICATION_FIELD_ERROR_INDEX] as AnyObject
+        let errorMessage = AUTHENTICATION_FIELD_ERROR_ENTRY["message"] as! String
+        let errorField = AUTHENTICATION_FIELD_ERROR_ENTRY["field"] as! String
+        self.alert(errorField, message: errorMessage, buttonText: "OK")
     }
     
     override func viewDidLoad() {
