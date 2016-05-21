4f4c97bf43f9c6318a4ae717248099f447bae3ca
Hesham Massoud <hesham.massoud@tum.de>
2016-05-21 12:01:44 +0200

    Refactor HomeViewController

--- RouteMe/HomeViewController.swift
+++ RouteMe/HomeViewController.swift
@@ -11,22 +11,11 @@ import UIKit
 class HomeViewController: UIViewController {
     
     @IBOutlet weak var usernameLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.hideKeyboardWhenTappedAround()
-        self.view.addBackground("bus_routeme.jpeg")
-        let isLoggedIn = NSUserDefaults.standardUserDefaults().boolForKey("isLoggedIn")
-        if (isLoggedIn) {
-            let loggedInUsername = NSUserDefaults.standardUserDefaults().valueForKey("username") as? String
-            usernameLabel.text = loggedInUsername;
-        }
-
-        // Do any additional setup after loading the view.
-    }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func logoutAction(sender: AnyObject) {
+        NSUserDefaults.standardUserDefaults().setBool(false, forKey: "isLoggedIn")
+        redirectToLoginScreen()
+        
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -36,20 +25,33 @@ class HomeViewController: UIViewController {
         }
     }
 
-    @IBAction func logoutAction(sender: AnyObject) {
-        NSUserDefaults.standardUserDefaults().setBool(false, forKey: "isLoggedIn")
-        redirectToLoginScreen()
-        
-    }
-    
     func redirectToLoginScreen() {
         dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            
             let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Login")
             self.presentViewController(viewController, animated: true, completion: nil)
         })
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
+        self.view.addBackground("bus_routeme.jpeg")
+        let isLoggedIn = NSUserDefaults.standardUserDefaults().boolForKey("isLoggedIn")
+        if (isLoggedIn) {
+            let loggedInId = NSUserDefaults.standardUserDefaults().valueForKey("loggedInId") as? String
+            let loggedInUsername = NSUserDefaults.standardUserDefaults().valueForKey("loggedInUsername") as? String
+            let loggedInEmail = NSUserDefaults.standardUserDefaults().valueForKey("loggedInEmail") as? String
+            let loggedInUser = User(id: loggedInId!, username: loggedInUsername!, email: loggedInEmail!)
+            usernameLabel.text = loggedInUser.username;
+        }
+        // Do any additional setup after loading the view.
+    }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
     /*
      // MARK: - Navigation
      
