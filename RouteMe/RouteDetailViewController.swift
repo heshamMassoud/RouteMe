bd4944900d74eab2dd5a66143a9a2f5c653adf22
Hesham Massoud <hesham.massoud@tum.de>
2016-05-25 19:40:26 +0200

    Refactor RouteDetailViewController from being a GOD class

--- RouteMe/RouteDetailViewController.swift
+++ RouteMe/RouteDetailViewController.swift
@@ -21,39 +21,46 @@ class RouteDetailViewController: UIViewController, UITableViewDelegate, UITableV
     @IBOutlet weak var routeDetailTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let mapView: GMSMapView = addMapView()
         
-        
-        
-        let camera = GMSCameraPosition.cameraWithLatitude(48.1622980, longitude: 11.5554340, zoom: 10)
-        
-        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        mapView.myLocationEnabled = true
-        self.view.addSubview(mapView)
-        
-        mapView.translatesAutoresizingMaskIntoConstraints = false
-        likeThisRouteLabel.translatesAutoresizingMaskIntoConstraints = false
-        likeThisRouteSwitch.translatesAutoresizingMaskIntoConstraints = false
-        
-        
-        mapView.leadingAnchor.constraintEqualToAnchor(self.view.leadingAnchor).active=true
-        mapView.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor).active = true
-        mapView.topAnchor.constraintEqualToAnchor(self.view.topAnchor).active = true
-        mapView.heightAnchor.constraintEqualToConstant(200).active = true
-        
-        likeThisRouteSwitch.topAnchor.constraintEqualToAnchor(mapView.bottomAnchor, constant: 14).active = true
-        likeThisRouteLabel.topAnchor.constraintEqualToAnchor(mapView.bottomAnchor, constant: 14).active = true
-        
-        routeDetailTableView.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor).active = true
-        
-        routeDetailTableView.delegate = self
-        routeDetailTableView.dataSource = self
+        positionLikeSwitch(mapView)
+        positionLikeLabel(mapView)
+        positionTableView()
         
         self.navigationController?.navigationBar.hidden = true
         
         drawRoutePath(mapView)
         drawMarker(mapView)
     }
-    
+
+    func addMapView() -> GMSMapView {
+        let camera = GMSCameraPosition.cameraWithLatitude(48.1622980, longitude: 11.5554340, zoom: 10)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        self.view.addSubview(mapView)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.leadingAnchor.constraintEqualToAnchor(self.view.leadingAnchor).active=true
+        mapView.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor).active = true
+        mapView.topAnchor.constraintEqualToAnchor(self.view.topAnchor).active = true
+        mapView.heightAnchor.constraintEqualToConstant(Style.Height.RouteDetailMapView).active = true
+        return mapView
+    }
+
+    func positionLikeSwitch(mapView: GMSMapView) {
+        likeThisRouteSwitch.translatesAutoresizingMaskIntoConstraints = false
+        likeThisRouteSwitch.topAnchor.constraintEqualToAnchor(mapView.bottomAnchor, constant: Style.Height.LikeSectionVerticalPadding).active = true
+    }
+
+    func positionLikeLabel(mapView: GMSMapView) {
+        likeThisRouteLabel.translatesAutoresizingMaskIntoConstraints = false
+        likeThisRouteLabel.topAnchor.constraintEqualToAnchor(mapView.bottomAnchor, constant: Style.Height.LikeSectionVerticalPadding).active = true
+    }
+
+    func positionTableView() {
+        routeDetailTableView.widthAnchor.constraintEqualToAnchor(self.view.widthAnchor).active = true
+        routeDetailTableView.delegate = self
+        routeDetailTableView.dataSource = self
+    }
+
     func drawRoutePath(mapView: GMSMapView) {
         let routePolyline = route.polyline
         let gmsPath = GMSPath(fromEncodedPath: routePolyline)
@@ -74,11 +81,18 @@ class RouteDetailViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Route details"
+        return Placeholder.Title.RouteDetails
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 100
+        return Style.Height.RouteDetailCells
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        clearCellContents(cell)
+        addContentsToCell(cell, index: indexPath.row)
+        return cell
     }
     
     func clearCellContents(cell: UITableViewCell) {
@@ -91,71 +105,99 @@ class RouteDetailViewController: UIViewController, UITableViewDelegate, UITableV
             }
         }
     }
-
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        clearCellContents(cell)
+    
+    func addContentsToCell(cell: UITableViewCell, index: Int) {
+        let currentRouteStep = route.steps[index]
+        let currentRouteStepTransportationMode = currentRouteStep[Key.RouteStep.TransportationMode] as! String
+        let currentRouteStepTransportationModeImagePath = Transportation.ImagePaths[currentRouteStepTransportationMode]
         
-        let currentRouteStep = route.steps[indexPath.row]
-        let currentRouteStepTransportationMode = currentRouteStep["transportationMode"] as! String
-        let currentRouteStepTransportationModeImagePath = Route.vehicleNamingMap[currentRouteStepTransportationMode]
+        let stepSummaryLabel = addStepSummaryToCell(cell)
+        let vehicleImageView = addVehicleImageViewToCell(cell, imagePath: currentRouteStepTransportationModeImagePath!)
+        let headsignLabel = addHeadsignLabel(cell, vehicleImageView: vehicleImageView)
         
-        let StepSummaryLabel = UILabel()
-        StepSummaryLabel.translatesAutoresizingMaskIntoConstraints = false
-        StepSummaryLabel.font = UIFont(name: "HelveticaNeue-Thin", size: 15)!
-        cell.addSubview(StepSummaryLabel)
-        StepSummaryLabel.topAnchor.constraintEqualToAnchor(cell.topAnchor, constant: 14).active = true
-        StepSummaryLabel.leadingAnchor.constraintEqualToAnchor(cell.leadingAnchor, constant: 14).active = true
-        StepSummaryLabel.trailingAnchor.constraintEqualToAnchor(cell.trailingAnchor, constant: 14).active = true
-        
-        let vehicleImage = UIImage(named: currentRouteStepTransportationModeImagePath!)
+        if route.isTransit {
+            addTransitRouteCellContents(cell, stepSummaryLabel: stepSummaryLabel, vehicleImageView: vehicleImageView, headsignLabel: headsignLabel)
+        } else {
+            addNonTransitRouteCellContets(cell, stepSummaryLabel: stepSummaryLabel, vehicleImageView: vehicleImageView, headsignLabel: headsignLabel, routeStep: currentRouteStep)
+        }
+    }
+    
+    func addStepSummaryToCell(cell: UITableViewCell) -> UILabel {
+        let stepSummaryLabel = UILabel()
+        stepSummaryLabel.translatesAutoresizingMaskIntoConstraints = false
+        stepSummaryLabel.font = Style.Font.RouteDetailCells
+        cell.addSubview(stepSummaryLabel)
+        stepSummaryLabel.topAnchor.constraintEqualToAnchor(cell.topAnchor, constant: 14).active = true
+        stepSummaryLabel.leadingAnchor.constraintEqualToAnchor(cell.leadingAnchor, constant: 14).active = true
+        stepSummaryLabel.trailingAnchor.constraintEqualToAnchor(cell.trailingAnchor, constant: 14).active = true
+        return stepSummaryLabel
+    }
+    
+    func addVehicleImageViewToCell(cell: UITableViewCell, imagePath: String) -> UIImageView {
+        let vehicleImage = UIImage(named: imagePath)
         let vehicleImageView = UIImageView(image: vehicleImage!)
         cell.addSubview(vehicleImageView)
         vehicleImageView.translatesAutoresizingMaskIntoConstraints = false
         vehicleImageView.leadingAnchor.constraintEqualToAnchor(cell.leadingAnchor, constant: 14).active = true
         vehicleImageView.widthAnchor.constraintEqualToConstant(28).active = true
         vehicleImageView.heightAnchor.constraintEqualToConstant(28).active = true
-        
+        return vehicleImageView
+    }
+    
+    func addHeadsignLabel(cell: UITableViewCell, vehicleImageView: UIImageView) -> UILabel {
         let headsignLabel = UILabel()
         headsignLabel.translatesAutoresizingMaskIntoConstraints = false
-        headsignLabel.font = UIFont(name: "HelveticaNeue-Thin", size: 15)!
+        headsignLabel.font = Style.Font.RouteDetailCells
         cell.addSubview(headsignLabel)
         headsignLabel.leadingAnchor.constraintEqualToAnchor(vehicleImageView.trailingAnchor, constant: 4).active = true
-        
-        if route.isTransit {
-            let stationsLabel = UILabel()
-            stationsLabel.translatesAutoresizingMaskIntoConstraints = false
-            stationsLabel.font = UIFont(name: "HelveticaNeue-Thin", size: 15)!
-            cell.addSubview(stationsLabel)
-            stationsLabel.topAnchor.constraintEqualToAnchor(StepSummaryLabel.bottomAnchor, constant: 7).active = true
-            stationsLabel.leadingAnchor.constraintEqualToAnchor(cell.leadingAnchor, constant: 14).active = true
-            
-            
-            vehicleImageView.topAnchor.constraintEqualToAnchor(stationsLabel.bottomAnchor, constant: 10).active=true
-            headsignLabel.topAnchor.constraintEqualToAnchor(stationsLabel.bottomAnchor, constant: 17).active=true
-            
-            StepSummaryLabel.text = route.summary
-            stationsLabel.text = "Münchner Freiheit - Marienplatz"
-            headsignLabel.text = "to Klinikium Großhadern"
-        } else {
-            
-            vehicleImageView.bottomAnchor.constraintEqualToAnchor(cell.bottomAnchor, constant: -10.5).active=true
-            headsignLabel.bottomAnchor.constraintEqualToAnchor(cell.bottomAnchor, constant: -14).active=true
-            
-            let currentRouteStepDistance = currentRouteStep["distance"] as! String
-            var currentRouteStepHTMLInstruction = currentRouteStep["htmlIntruction"] as! String
-            currentRouteStepHTMLInstruction = "<span style='font-family: HelveticaNeue-Thin !important; font-size: 15px'>\(currentRouteStepHTMLInstruction)"
-            currentRouteStepHTMLInstruction = currentRouteStepHTMLInstruction.stringByReplacingOccurrencesOfString("<b>", withString: "<span style='font-family: HelveticaNeue-Light !important; font-size: 15px'>")
-            currentRouteStepHTMLInstruction = currentRouteStepHTMLInstruction.stringByReplacingOccurrencesOfString("</b>", withString: "</span>")
-            currentRouteStepHTMLInstruction = "\(currentRouteStepHTMLInstruction)</span>"
-            let attrStr = try! NSMutableAttributedString(
-                data: currentRouteStepHTMLInstruction.dataUsingEncoding(NSUnicodeStringEncoding, allowLossyConversion: true)!,
-                options: [ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
-                documentAttributes: nil)
-            StepSummaryLabel.attributedText = attrStr
-            StepSummaryLabel.numberOfLines = 2
-            headsignLabel.text = currentRouteStepDistance
-        }
-        return cell
+        return headsignLabel
     }
+    
+    func addTransitRouteCellContents(cell: UITableViewCell, stepSummaryLabel: UILabel, vehicleImageView: UIImageView, headsignLabel: UILabel) {
+        let stationsLabel = addStationsLabel(cell, stepSummaryLabel: stepSummaryLabel)
+        vehicleImageView.topAnchor.constraintEqualToAnchor(stationsLabel.bottomAnchor, constant: 10).active=true
+        headsignLabel.topAnchor.constraintEqualToAnchor(stationsLabel.bottomAnchor, constant: 17).active=true
+        stepSummaryLabel.text = route.summary
+        headsignLabel.text = "to Klinikium Großhadern"
+    }
+    
+    func addStationsLabel(cell: UITableViewCell, stepSummaryLabel: UILabel) -> UILabel{
+        let stationsLabel = UILabel()
+        stationsLabel.translatesAutoresizingMaskIntoConstraints = false
+        stationsLabel.font = Style.Font.RouteDetailCells
+        stationsLabel.text = "Münchner Freiheit - Marienplatz"
+        cell.addSubview(stationsLabel)
+        stationsLabel.topAnchor.constraintEqualToAnchor(stepSummaryLabel.bottomAnchor, constant: 7).active = true
+        stationsLabel.leadingAnchor.constraintEqualToAnchor(cell.leadingAnchor, constant: 14).active = true
+        return stationsLabel
+    }
+    
+    func addNonTransitRouteCellContets(cell: UITableViewCell, stepSummaryLabel: UILabel, vehicleImageView: UIImageView,
+                                       headsignLabel: UILabel, routeStep: AnyObject) {
+        vehicleImageView.bottomAnchor.constraintEqualToAnchor(cell.bottomAnchor, constant: -10.5).active=true
+        headsignLabel.bottomAnchor.constraintEqualToAnchor(cell.bottomAnchor, constant: -14).active=true
+        let currentRouteStepDistance = routeStep[Key.RouteStep.NonTransit.Distance] as! String
+        let currentRouteStepHTMLInstruction = routeStep[Key.RouteStep.NonTransit.HTMLInstruction] as! String
+        setHTMLInstructionLabelText(stepSummaryLabel, htmlInstruction: currentRouteStepHTMLInstruction)
+        headsignLabel.text = currentRouteStepDistance
+    }
+    
+    func setHTMLInstructionLabelText(label: UILabel, htmlInstruction: String) {
+        let styledHTMLInstruction = styleHTMLInstruction(htmlInstruction)
+        let attrStr = try! NSMutableAttributedString(
+            data: styledHTMLInstruction.dataUsingEncoding(NSUnicodeStringEncoding, allowLossyConversion: true)!,
+            options: [ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
+            documentAttributes: nil)
+        label.attributedText = attrStr
+        label.numberOfLines = 2
+    }
+    
+    func styleHTMLInstruction(htmlInstruction: String) -> String {
+        var styledHTMLInstruction = "\(Style.HTML.InstructionSpanTagOpenning)\(htmlInstruction)"
+        styledHTMLInstruction = styledHTMLInstruction.stringByReplacingOccurrencesOfString(Style.HTML.InstructionBoldTagOpenning, withString: Style.HTML.InstructionBoldSpanTagOpenning)
+        styledHTMLInstruction = styledHTMLInstruction.stringByReplacingOccurrencesOfString(Style.HTML.InstructionBoldTagClosing, withString: Style.HTML.InstructionSpanTagClosing)
+        styledHTMLInstruction = "\(styledHTMLInstruction)\(Style.HTML.InstructionSpanTagClosing)"
+        return styledHTMLInstruction
+    }
+    
 }
