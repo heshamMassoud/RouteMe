b64345fbcd2d48752de0f4dc9834823f554fb72e
Hesham Massoud <hesham.massoud@tum.de>
2016-05-25 20:56:09 +0200

    Refactor Autocomplete view controller

--- RouteMe/SearchResultsController.swift
+++ RouteMe/SearchResultsController.swift
@@ -13,102 +13,43 @@ protocol InsertAddressOnTextfield{
 }
 
 class SearchResultsController: UITableViewController {
-    
-    var searchResults: [String]!
+
+    var autocompleteResults: [String]!
     var isEditingStartPoint: Bool!
     var delegate: InsertAddressOnTextfield!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.searchResults = Array()
+        self.autocompleteResults = Array()
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cellIdentifier")
         
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    // MARK: - Table view data source
-    
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        
-        return 1
-    }
-    
+
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return self.searchResults.count
+        return self.autocompleteResults.count
     }
-    
-    
+
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cellIdentifier", forIndexPath: indexPath)
-        
-        cell.textLabel?.text = self.searchResults[indexPath.row]
+        cell.textLabel?.text = self.autocompleteResults[indexPath.row]
         return cell
     }
-    
-    
-    
+
     override func tableView(tableView: UITableView,
                             didSelectRowAtIndexPath indexPath: NSIndexPath){
         self.dismissViewControllerAnimated(true, completion: nil)
-        let address: String = self.searchResults[indexPath.row]
+        let address: String = self.autocompleteResults[indexPath.row]
         self.delegate.InsertAddress(address, isStart: isEditingStartPoint)
     }
-    
-    
+
     func reloadDataWithArray(array:[String], isStart: Bool){
-        self.searchResults = array
+        self.autocompleteResults = array
         self.tableView.reloadData()
         self.isEditingStartPoint = isStart
     }
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-     if editingStyle == .Delete {
-     // Delete the row from the data source
-     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-     } else if editingStyle == .Insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
